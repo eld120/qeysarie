4d17fb212642b42ab4881f701e441fecdb0f4d80
Seyamack Afzali <seyamack@gmail.com>
2024-08-06 23:21:23 -0500

    rough idea of the remainder of the homepage

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function HeroSection() {
+    // You may be interested in list
+    // const randomStocks 
+
+    return (
+        <div>
+            <h4>You may be interested in</h4>
+            <div>
+                <ul>
+
+                </ul>
+                <div>
+                    <div>
+                        <p>Create a portfolio to view your investments in one place</p>
+                        <button>New Portfolio</button>
+                    </div>
+                    <div>
+                        <h4>Market trends</h4>
+                        <button>Market indexes</button>
+                        <button>Most active</button>
+                        <button>Gainers</button>
+                        <button>Losers</button>
+                        <button>Climate Leaders</button>
+                        <button>Crypto</button>
+                        <button>Currencies</button>
+                    </div>
+                    <div>
+                        <h4>Earnings calendar</h4>
+                        <p>Based on popular stocks</p>
+                        <ul></ul>
+                    </div>
+                    <div><h4>Most followed on Google</h4>
+                        <ul></ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default HeroSection
