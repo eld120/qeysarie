6d5d0435bf0953a8d4e789b560268c2a47c5412d
Seyamack Afzali <seyamack@gmail.com>
2024-08-30 15:05:00 -0500

    Modified source of data, transformed new data to fit previous component. ugly but works

--- app/components/NewSearchBar.tsx
+++ app/components/NewSearchBar.tsx
@@ -1,19 +1,99 @@
 'use client'
 import { useEffect, useState } from "react"
 import { QueryFunctionContext, useQuery, useQueryClient } from "@tanstack/react-query"
-import { StockCard, StockData } from "./StockCard"
-
+import { StockData, StockCard } from "./StockCard"
+import { StockPriceData } from "./New/StockPriceCard"
 interface SearchInputProps {
-    stockArray?: StockData[]
+    stockArray?: StockPriceData[]
 }
-async function getStocks(query: string): Promise<StockData[]> {
-    const response = await fetch('/stock_data.json', { cache: 'default' })
+interface Stock {
+    name: string
+    slug: string
+    ticker: string
+}
+interface IntermediateStockData {
+    date: string
+    price: string
+    stock: Stock
+    volume: number
+}
+function transformStockPriceData(stockArray: IntermediateStockData[]) {
+    const newStockMap: any = {}
+    const finalArray: StockData[] = []
+    stockArray.forEach((obj: IntermediateStockData) => {
+        if (!newStockMap[obj.stock.ticker]) {
+            newStockMap[obj.stock.ticker] = {}
+        }
+
+        newStockMap[obj.stock.ticker][obj.date] = {
+            company: obj.stock.name,
+            price: obj.price,
+            volume: obj.volume
+
+        }
+
+        if (Object.keys(newStockMap[obj.stock.ticker]).length > 1) {
+            const [firstDate, secondDate] = Object.keys(newStockMap[obj.stock.ticker])
+            if (new Date(firstDate) > new Date(secondDate)) {
+                const dollarDiff = parseFloat(newStockMap[obj.stock.ticker][firstDate].price) - parseFloat(newStockMap[obj.stock.ticker][secondDate].price)
+                const percentageDiff = dollarDiff / newStockMap[obj.stock.ticker][secondDate].price
+                newStockMap[obj.stock.ticker][obj.date] = {
+                    company: obj.stock.name,
+                    price: obj.price,
+                    volume: obj.volume,
+                    percentage: (percentageDiff * 100).toFixed(2),
+                    change: dollarDiff
+                }
+
+                finalArray.push({
+                    company: obj.stock.name,
+                    price: obj.price,
+                    date: obj.date,
+                    volume: obj.volume,
+                    percentage: (percentageDiff * 100).toFixed(2),
+                    change: dollarDiff
+                })
+            } else {
+                const dollarDiff = parseFloat(newStockMap[obj.stock.ticker][secondDate].price) - parseFloat(newStockMap[obj.stock.ticker][firstDate].price)
+                const percentageDiff = dollarDiff / newStockMap[obj.stock.ticker][firstDate].price
+                newStockMap[obj.stock.ticker][obj.date] = {
+                    company: obj.stock.name,
+                    ticker: obj.stock.ticker,
+                    price: parseFloat(obj.price),
+                    percentage: (percentageDiff * 100).toFixed(2),
+                    change: dollarDiff,
+                    volume: obj.volume
+                }
+
+                finalArray.push({
+                    company: obj.stock.name,
+                    ticker: obj.stock.ticker,
+                    price: parseFloat(obj.price),
+                    percentage: (percentageDiff * 100).toFixed(2),
+                    change: dollarDiff,
+                    volume: obj.volume
+                })
+
+            }
+
+
+        }
+    })
+    return finalArray
+
+}
+async function getStocks(query: string): Promise<StockPriceData[]> {
+    const response = await fetch(`http://localhost:8000/api/v1.0/stock-prices/?name=${query}&ticker=${query}`, {
+        cache: 'default',
+    })
     const data = await response.json()
 
-    return data?.filter((stock: StockData) => {
+
+    return transformStockPriceData(data.results).filter((obj: StockData) => {
         const normalizedInput = query.toLowerCase()
 
-        return stock?.ticker?.toLowerCase().includes(normalizedInput) || stock?.company?.toLowerCase().includes(normalizedInput)
+        return obj?.ticker.toLowerCase().includes(normalizedInput) || obj?.company.toLowerCase().includes(normalizedInput)
+
     })
 
 }
@@ -39,7 +119,7 @@ export default function NewSearchBar() {
     return (
         <>
             <input type="text" placeholder="Search by ticker or name" value={query} onChange={handleInputChange} className="w-full rounded-md border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
-
+            {query}
             {!debounceQuery ? (<div></div>) : (
                 <div className="container mx-auto mt-3 flex flex-wrap gap-3">
 
@@ -49,6 +129,7 @@ export default function NewSearchBar() {
                         data?.map((stock) => (
                             <div key={stock.ticker}>
                                 <StockCard {...stock} />
+
                             </div>
                         ))
                     ) : (
--- app/components/StockCard.tsx
+++ app/components/StockCard.tsx
@@ -10,18 +10,18 @@ export interface StockData {
     volume: number
 }
 
-export const StockCard: React.FC<StockData> = (stock: StockData) => {
-    const stockColor = stock.change >= 0 ? 'text-green-500' : 'text-red-500'
+export const StockCard: React.FC<StockData> = (priceData: StockData) => {
+    const stockColor = priceData.change >= 0 ? 'text-green-500' : 'text-red-500'
 
-
+    // console.log(priceData)
     return (
         <div className='bg-white rounded-lg shadow p-2 max-w-44 min-w-20'>
             <div className='flex'>
                 <div className="flex flex-col">
-                    <h3 className='text-sm font-medium'> {stock.ticker} {/*: {company} */}</h3>
-                    <p className="text-gray-500 text-sm">${stock.price}</p>
+                    <h3 className='text-sm font-medium'> {priceData.ticker} : {priceData.company}</h3>
+                    <p className="text-gray-500 text-sm">${priceData.price}</p>
                 </div>
-                <p className={`text-sm font-semibold ${stockColor}`}>{stock.percentage}%</p>
+                <p className={`text-sm font-semibold ${stockColor}`}> {priceData.percentage}%</p>
             </div>
 
         </div>
