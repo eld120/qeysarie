f087019268d9894b11a3ab0a38e49cad7a5cf731
Seyamack Afzali <seyamack@gmail.com>
2024-09-11 15:10:30 -0500

    pulling queries out of components

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,50 @@
-'use client'
 
+'use client'
+import { GetServerSideProps } from "next";
+import { useQuery } from '@tanstack/react-query';
 import Header from "./components/Header";
-import SearchInput from "./components/BasicSearchBar";
+
 import NewSearchBar from "./components/NewSearchBar";
 import MarketList from "./components/MarketsList";
 import HeroSection from "./components/HeroSection";
 import MarketTrends from "./components/MarketTrends";
+import { StockData } from "./components/StockCard";
+
+async function getTodaysStockPrices(): Promise<StockData[]> {
+  const response = await fetch(`http://localhost:8088/api/v1.0/stock-prices/get_today/`, {
+    cache: 'default',
+  })
+  const data = await response.json()
 
 
+  return data
+
+}
 
 
 export default function Home() {
 
+
+  const { isLoading, error, data } = useQuery<StockData[], Error>({
+    queryKey: ['todayStocks'],
+    queryFn: getTodaysStockPrices
+  })
+
+
+  if (isLoading) {
+    return (
+      <div>
+        loading...
+      </div>
+    )
+  }
   return (
     <>
       <Header />
 
       <MarketList />
       <NewSearchBar />
-      <HeroSection />
+      <HeroSection data={data.results} />
       <MarketTrends />
     </>
   );
