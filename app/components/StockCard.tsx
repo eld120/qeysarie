80e07bdf8720f629e374180617c011353e1d5ac0
Seyamack Afzali <seyamack@gmail.com>
2024-10-18 01:15:40 -0500

    a bunch of ui updates that are long overdue

--- app/components/StockCard.tsx
+++ app/components/StockCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import randomColor from 'randomcolor'
 
 export interface StockData {
     company: string
@@ -13,19 +14,19 @@ export interface StockData {
 
 export const StockCard: React.FC<StockData> = (priceData: StockData) => {
     const stockColor = priceData.change >= 0 ? 'text-green-500' : 'text-red-500'
-
+    const color = randomColor({ luminosity: 'dark' });
 
     return (
-        <div className='bg-white rounded-lg shadow p-2 w-44 h-24'>
+        <div className='bg-white rounded-lg shadow w-44 h-24'>
             <div className='flex'>
                 <div className="flex flex-col">
-                    <h3 className='text-sm font-medium'>{priceData.ticker} : {priceData.company}</h3>
+                    <h3 className='text-xs font-medium '><div className='w-12 h-6 py-1 rounded text-white text-center' style={{ backgroundColor: color }}>{priceData.ticker}</div>  {priceData.company}</h3>
                     <p className="text-gray-500 text-sm">${priceData.price}</p>
                 </div>
-                <p className={`text-sm font-semibold ${stockColor}`}>{priceData.percentage}%</p>
+                <p className={`text-sm font-semibold self-center ${stockColor}`}>{priceData.percentage}%</p>
             </div>
 
-        </div>
+        </div >
     )
 }
 
--- app/components/StockListComponent.tsx
+++ app/components/StockListComponent.tsx
@@ -1,20 +1,30 @@
 import React from 'react'
 import Link from 'next/link'
 import { StockData } from './StockCard'
-
+import randomColor from 'randomcolor';
+const greenBG = "#e6f4ea"
+const greenText = "#137333"
+const redText = "#a50e0e"
+const redBG = "#fce8e6"
 
 function StockListComponent(props: StockData) {
-
+  const color = randomColor({ luminosity: 'dark' });
+  const bgColor = props.price < 0 ? redBG : greenBG
+  const txtColor = props.price < 0 ? redText : greenText
   return (
-    <li className={'border-t-2 flex gap-10 px-10'} key={props.ticker}>
-      {/* <Link href={`/quote/${encodeURIComponent(props.ticker)}`}> */}
+    <li className={'border-t-2 border-slate-100'} key={props.ticker}>
 
-      <div className=''>{props.ticker}</div>
-      <div className=''>{props.company}</div>
-      <div className=''>${props.price}</div>
-      <div className=''>${props.change}</div>
-      <div className=''>{props.percentage}</div>
-      {/* </Link> */}
+      <Link href={`/quote/${encodeURIComponent(props.ticker)}`} className='flex items-center justify-between p-3'>
+        <div>
+          <div className="text-white w-12 h-6 text-xs rounded text-center font-medium py-1" style={{ backgroundColor: color }}>{props.ticker}</div>
+          <div className='font-light text-sm'>{props.company}</div>
+        </div>
+        <div className='flex gap-2'>
+          <div className='font-medium'>${props.price}</div>
+          <div className='font-medium w-12 h-8 rounded text-center py-1' style={{ backgroundColor: bgColor, color: txtColor }}>${props.change}</div>
+          <div className='font-medium w-12 h-8 rounded text-center py-1' style={{ backgroundColor: bgColor, color: txtColor }}>{props.percentage}%</div>
+        </div>
+      </Link>
     </li >
   )
 }
